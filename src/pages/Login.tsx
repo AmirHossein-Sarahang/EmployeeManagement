ae3efc6a7e146e2daa3f9ba457edbfc237eef1e9
AmirHossein <AmirSarahng@gmail.com>
2025-01-16 23:15:41 +0330

    Update Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [Email, setEmail] = useState('')
+    const [Password, setPassword] = useState('')
+
+    const handleEmailInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value)
+    }
+    const handleSubmit = () => {
+        console.log(Email,Password)
+    }
+    const isSubmitDisabled = () => {
+        if(!Email || !Password){
+            return true
+        }else {
+            return false
+        }
+    }
     return (
         <div className="flex flex-col w-full items-center justify-center h-screen">
             <div className="flex flex-col w-2/12 h-2/6 justify-between border border-gray-300 shadow rounded bg-gray-50">
@@ -9,15 +31,15 @@ const Login = () => {
                     
                     <div className="flex flex-col mr-5 ml-5">
                         <label htmlFor="Email">Email:</label>
-                        <input placeholder="Enter your email..." type="email" className=" border rounded p-1" />
+                        <input onChange={handleEmailInput} placeholder="Enter your email..." type="email" className=" border rounded p-1" />
                     </div>
                     <div className="flex flex-col mr-5 ml-5">
                         <label htmlFor="Password">Password:</label>
-                        <input placeholder="Enter your password..." type="password" className="border rounded p-1"/>
+                        <input onChange={handlePasswordInput} placeholder="Enter your password..." type="password" className="border rounded p-1"/>
                     </div>
                 </div>
 
-                <button className="mr-7 ml-7 mb-3 bg-blue-500 rounded text-white">Submit</button>
+                <button disabled={isSubmitDisabled()} onClick={handleSubmit} className="mr-7 ml-7 mb-3 bg-blue-500 rounded text-white">Submit</button>
             </div>
         </div>
     );
