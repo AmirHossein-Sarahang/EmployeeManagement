5c90fecd8ea654546a9d82b2e11a7eabb8a62a33
AmirHossein <AmirSarahng@gmail.com>
2025-01-16 18:25:18 +0330

    update Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,26 @@
-const Login  = () =>{
+const Login = () => {
+    return (
+        <div className="flex flex-col w-full items-center justify-center h-screen">
+            <div className="flex flex-col w-2/12 h-2/6 justify-between border border-gray-300 shadow rounded bg-gray-50">
+                <div className="flex flex-col gap-4">
+                    <div className="flex justify-center text-blue-500">
+                        <h1>Login</h1>
+                    </div>
+                    
+                    <div className="flex flex-col mr-5 ml-5">
+                        <label htmlFor="Email">Email:</label>
+                        <input placeholder="Enter your email..." type="email" className=" border rounded p-1" />
+                    </div>
+                    <div className="flex flex-col mr-5 ml-5">
+                        <label htmlFor="Password">Password:</label>
+                        <input placeholder="Enter your password..." type="password" className="border rounded p-1"/>
+                    </div>
+                </div>
 
-    return(
-        <>
-            <div>amir</div>
-        </>
-    )
-}
+                <button className="mr-7 ml-7 mb-3 bg-blue-500 rounded text-white">Submit</button>
+            </div>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
