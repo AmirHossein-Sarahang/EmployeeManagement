1cebcf4fe7ebfc76bf58ab66f3a23b44d0e046bc
AmirHossein <AmirSarahng@gmail.com>
2025-01-17 21:36:54 +0330

    Update App : add Dashboard path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Login from "./pages/Login"
-
+import Dashboard from "./pages/Dashboard"
 function App() {
   const router = createBrowserRouter([
     {
       path:'/Login',
       element: <Login/>
+    },
+    {
+      path:'/Dashboard',
+      element: <Dashboard/>
     }
   ])
   return (
