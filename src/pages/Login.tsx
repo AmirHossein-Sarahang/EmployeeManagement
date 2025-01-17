484836fc3c8ade2b5df030d4b7c3149c2fac25ab
AmirHossein <AmirSarahng@gmail.com>
2025-01-17 19:45:28 +0330

    Update Login use Input Form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,42 +1,42 @@
-import { useState } from "react";
+import { useState} from "react";
+import users from "../constant/user.json"
+import InputForm from "../components/common/InputForm";
 
 const Login = () => {
-    const [Email, setEmail] = useState('')
-    const [Password, setPassword] = useState('')
+    const [username, setusername] = useState('')
+    const [password, setpassword] = useState('')
 
     const handleEmailInput = (e:React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value)
+        setusername(e.target.value)
     }
 
     const handlePasswordInput = (e:React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
+        setpassword(e.target.value)
     }
     const handleSubmit = () => {
-        console.log(Email,Password)
+        const existuser = users.filter((user)=>(
+           (user.username === username || user.email === username ) && user.password === password)
+        )
+        console.log(existuser);
+
     }
     const isSubmitDisabled = () => {
-        if(!Email || !Password){
+        if(!username || !password){
             return true
         }else {
             return false
         }
     }
+
     return (
         <div className="flex flex-col w-full items-center justify-center h-screen">
-            <div className="flex flex-col w-2/12 h-2/6 justify-between border border-gray-300 shadow rounded bg-gray-50">
+            <div className="flex flex-col w-3/12 h-2/6 justify-between border border-gray-300 shadow rounded bg-gray-50 min-w-[350px]">
                 <div className="flex flex-col gap-4">
                     <div className="flex justify-center text-blue-500">
                         <h1>Login</h1>
                     </div>
-                    
-                    <div className="flex flex-col mr-5 ml-5">
-                        <label htmlFor="Email">Email:</label>
-                        <input onChange={handleEmailInput} placeholder="Enter your email..." type="email" className=" border rounded p-1" />
-                    </div>
-                    <div className="flex flex-col mr-5 ml-5">
-                        <label htmlFor="Password">Password:</label>
-                        <input onChange={handlePasswordInput} placeholder="Enter your password..." type="password" className="border rounded p-1"/>
-                    </div>
+                    <InputForm onChange={handleEmailInput} type="Email" lable="Username or email:"></InputForm>
+                    <InputForm onChange={handlePasswordInput} type="Password" lable="Password:"></InputForm>
                 </div>
 
                 <button disabled={isSubmitDisabled()} onClick={handleSubmit} className="mr-7 ml-7 mb-3 bg-blue-500 rounded text-white">Submit</button>
